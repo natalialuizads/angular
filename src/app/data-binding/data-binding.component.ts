70d318b6b74fc2939d01e7220d86c297f06d3c71
Natália Luiza <natalialuizas@live.com>
2021-09-25 10:50:32 -0300

    Data Binding - Exemplos

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Position { 
+  x: number,
+  y: number,
+}
+
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
@@ -7,17 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
   public interpolation: string = 'Estou aprendendo angular :)';
+
   public disableButton: boolean = true;
+
   public imagemUrl: string =
     'https://freepngimg.com/thumb/dog/56-dog-png-image.png';
+
+  public imagemTitle: string = 'Imagem de um cachorrinho';
+
   public nome: string = 'Natália Luiza';
+
   public idade: number = 26;
+
+  public position: Position = {x: 0, y: 0};
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  onClick(): void { 
-    console.log('Botão foi clicado');
+  /**
+   * Para ver todos os eventos procurar a interface
+   * GlobalEventHandlersEventMap dentro do arquivo
+   * lib.dom.d.ts
+   */
+  public onClick(texto: string): void { 
+   alert(texto)
+  }
+
+  public onMouseMove(event: MouseEvent): void {
+   this.position = { x: event.offsetX, y: event.offsetY };
   }
 }
