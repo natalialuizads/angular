244c831a2e1fcb9fdadb6bab2572a7b3b91c4986
Natália Luiza <natalialuizas@live.com>
2021-09-28 22:50:24 -0300

    Diretivas Estruturais - Exemplos

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -3,19 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.scss']
+  styleUrls: ['./directives.component.scss'],
 })
 export class DirectivesComponent implements OnInit {
   public condition: boolean = true;
+
   public conditionClick: boolean = false;
-  constructor() { }
+
+  public nome: string = 'Natália';
+
+  public list: Array<{ nome: string; idade: number }> = [
+    { nome: 'Natália ', idade: 26 },
+    { nome: 'Brenda', idade: 24 },
+    { nome: 'Apolo ', idade: 2 },
+  ];
+
+  constructor() {}
 
   ngOnInit(): void {
-    setInterval(() => { if(this.condition) this.condition = false }, 2000)
+    setInterval(() => {
+      if (this.condition) this.condition = false;
+    }, 2000);
   }
 
-  public onClick(){
-    this.conditionClick = !this.conditionClick 
+  public onClick() {
+    this.conditionClick = !this.conditionClick;
   }
 
+  public onClickAddList() {
+    this.list.push({ nome: 'João', idade: 35 });
+  }
+
+  public onClickEventList(number: number) {
+    this.list.splice(number, 1)
+  }
 }
