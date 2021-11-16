db8847ea856e7fd7c9b07b89246594a4aaab612b
Natalia Luiza <natalialuizas@live.com>
2021-11-16 11:25:16 -0300

    exemplos reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <app-forms></app-forms>
+  <app-reactive-forms></app-reactive-forms>
   `,
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
 import { LifecycleComponent } from './lifecycle/lifecycle.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DirectivesComponent } from './directives/directives.component';
 import { PipesComponent } from './pipes/pipes.component';
 import { ModulesModule } from './modules/modules.module';
 import { BankingComponent } from './unit-tests/banking/banking.component';
 import { InputOutputModule } from './input-output/input-output.module';
 import { FormsComponent } from './forms/forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +24,14 @@ import { FormsComponent } from './forms/forms.component';
     PipesComponent,
     BankingComponent,
     FormsComponent,
+    ReactiveFormsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ModulesModule,
     InputOutputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
